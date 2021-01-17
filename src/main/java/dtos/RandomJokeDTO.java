7c4203a384ef8c6d37d7e41ce6eb3fb82b61e963
Rasmus Grønbek <groenbek@live.dk>
2021-01-17 16:00:11 +0100

    test

---
+++ src/main/java/dtos/RandomJokeDTO.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package dtos;
+
+/**
+ *
+ * @author groen
+ */
+public class RandomJokeDTO {
+
+    private String value;
+
+    public RandomJokeDTO(String value) {
+        this.value = value;
+    }
+
+    public String getValue() {
+        return value;
+    }
+
+    public void setValue(String value) {
+        this.value = value;
+    }
+
+    public RandomJokeDTO() {
+    }
+}
